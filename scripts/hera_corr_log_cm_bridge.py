932e812188244b0c7179938201bc9d16684128ee
jack <jackh@berkeley>
2019-06-18 20:51:04 +0000

    Add option to refilter on log level before sending to CM
    Primarily added so that Xtor info messages can go to redis but not CM

--- scripts/hera_corr_log_cm_bridge.py
+++ scripts/hera_corr_log_cm_bridge.py
@@ -5,12 +5,24 @@ from hera_corr_cm import helpers
 from hera_mc import mc
 from astropy.time import Time
 
+allowed_levels = ["DEBUG", "INFO", "NOTIFY", "WARNING", "ERROR", "CRITICAL"]
+logging.addLevelName(logging.INFO+1, "NOTIFY")
+
 parser = argparse.ArgumentParser(description='Subscribe to the redis-based log stream',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-r', dest='redishost', type=str, default='redishost',
                     help ='Host servicing redis requests')
+parser.add_argument('-l', dest='level', type=str, default="NOTIFY",
+                    help ='Don\'t log messages below this level. Allowed values are %s' % allowed_levels)
 
 args = parser.parse_args()
+
+if args.level not in allowed_levels:
+    print "Selected log level not allowed. Allowed levels are:", allowed_levels
+    print "Defaulting to WARNING"
+    level = logging.WARNING
+else:
+    level = logging.getLevelName(args.level)
 
 logger = helpers.add_default_log_handlers(logging.getLogger(__file__))
 
@@ -36,9 +48,11 @@ while(True):
                 decoded = json.loads(mess["data"])
                 msg = decoded['formatted']
                 logtime = self.Time(time.time(), format="unix")
-                # Re-code level because HeraMC logs 1 as most severe, and python logging calls critical:50, debug:10
-                severity = max(1, 100 / decoded['levelno'])
-                session.add_subsystem_error(logtime, subsystem, severity, msg, testing=False)
+                msg_level = decoded['levelno']
+                if msg_level >= level:
+                    # Re-code level because HeraMC logs 1 as most severe, and python logging calls critical:50, debug:10
+                    severity = max(1, 100 / decoded['levelno'])
+                    session.add_subsystem_error(logtime, subsystem, severity, msg, testing=False)
             except:
                 pass
     except KeyboardInterrupt:
