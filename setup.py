7a9225daeed83d51aaaf569c1e89f094c8094d16
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-10-16 12:56:22 -0700

    format print statements

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ try:
     ver = VERSION + '-' + subprocess.check_output(['git', 'describe', '--abbrev=8', '--always', '--dirty', '--tags']).strip().decode('utf-8')
 except:
     ver = VERSION
-    print(('Couldn\'t get version from git. Defaulting to %s' % ver))
-print('Version is: %s' % ver)
+    print(('Couldn\'t get version from git. Defaulting to {ver:s}'.format(ver=ver)))
+print('Version is: {ver=s}'.format(ver=ver))
 
 # Generate a __version__.py file with this version in it
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'hera_corr_cm', '__version__.py'), 'w') as fh:
-    fh.write('__version__ = "%s"' % ver)
+    fh.write('__version__ = "{ver:s}"'.format(ver=ver))
 
 PACKAGES = find_packages()
 print(PACKAGES)
