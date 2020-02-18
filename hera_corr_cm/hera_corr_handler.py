fd49ccfb449bca0e90fe43a00df9ba3b494fc88e
Aaron Parsons <aparsons@berkeley.edu>
2020-02-18 04:32:36 +0000

    Changed feng_init call in hera_corr_handler.py to use -p instead of -P to avoid reprogramming FPGAs that don't need it. This may permit a partially booted correlator to successfully boot the rest of the way.

--- hera_corr_cm/hera_corr_handler.py
+++ hera_corr_cm/hera_corr_handler.py
@@ -134,13 +134,15 @@ class HeraCorrHandler(object):
     def _xtor_up(self, input_power_target=None, output_rms_target=None):
 
         # For BDA snap_init has to happen first to ensure the antennas in the config file are correct.
-        self.logger.info("Issuing hera_snap_feng_init.py -P -s -e -i --noredistapcp --nomultithread")
+        #self.logger.info("Issuing hera_snap_feng_init.py -P -s -e -i --noredistapcp --nomultithread")
+        self.logger.info("Issuing hera_snap_feng_init.py -p -s -e -i --noredistapcp --nomultithread")
         proc3 = Popen(["ssh",
                        "{user:s}@{host:s}".format(user=SNAP_USER, host=SNAP_HOST),
                        "source", SNAP_ENVIRONMENT,
                        "&&",
                        "hera_snap_feng_init.py",
-                       "-P", "-s", "-e", "-i", "--noredistapcp", "--nomultithread"])
+                       #"-P", "-s", "-e", "-i", "--noredistapcp", "--nomultithread"])
+                       "-p", "-s", "-e", "-i", "--noredistapcp", "--nomultithread"])
         proc3.wait()
         if int(proc3.returncode) != 0:
             self.logger.error("Error running hera_snap_feng_init.py")
